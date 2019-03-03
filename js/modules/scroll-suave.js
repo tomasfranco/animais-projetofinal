e7c8012308d2f88fb6b9d274763abb9808cbbafa
Tomás Franco <tomas.f.carvalho@gmail.com>
2019-03-03 20:59:12 -0300

    ScrollSuave transformado em classe

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,30 +1,35 @@
 
-export default function initScrollSuave() {
-  const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]');
+export default class ScrollSuave {
+  constructor(links, options) {
+    this.linksInternos = document.querySelectorAll(links);
+    if (options === undefined) {
+      this.options = { behavior: 'smooth', block: 'start' };
+    } else {
+      this.options = options;
+    }
 
-  function scrollToSection(event) {
+    this.scrollToSection = this.scrollToSection.bind(this);
+  }
+
+
+  scrollToSection(event) {
     event.preventDefault();
     const href = event.currentTarget.getAttribute('href');
     const section = document.querySelector(href);
     console.log(section);
-    section.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-
-    });
-
-  /*
-  Forma alternartiva de srcoll smooth
-      const topo = section.offsetTop;
-
-      window.scrollTo({
-        top: topo,
-        behavior: 'smooth',
-      });
-      */
+    section.scrollIntoView(this.options);
   }
 
-  linksInternos.forEach((link) => {
-    link.addEventListener('click', scrollToSection);
-  });
+  addLinkEvent() {
+    this.linksInternos.forEach((link) => {
+      link.addEventListener('click', this.scrollToSection);
+    });
+  }
+
+  init() {
+    if (this.linksInternos.length) {
+      this.addLinkEvent();
+    }
+    return this;
+  }
 }
