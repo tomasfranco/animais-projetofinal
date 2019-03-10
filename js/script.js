a29c68d1c1cfba4d5cab4593f69156dc24434be7
Tomás Franco <tomas.f.carvalho@gmail.com>
2019-03-10 15:56:21 -0300

    fetch animais refatorado

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ import Tooltip from './modules/tooltip.js';
 import initDropDownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
-import iniFetchAnimais from './modules/fetch-animais.js';
+import fetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
 
 
@@ -29,6 +29,6 @@ tooltip.init();
 initDropDownMenu();
 initMenuMobile();
 initFuncionamento();
-iniFetchAnimais();
+fetchAnimais('animaisapi.json', '.numeros-grid');
 initFetchBitcoin();
 initAnimacaoScroll();
